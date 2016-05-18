8662dd3823fa527f9b9dbee7a2523dac5d89be64
Stefan Hekele <sh@plan2.net>
2016-05-18 18:02:39 +0200

    Fix incorrect class reference

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,11 +4,11 @@ defined('TYPO3_MODE') || die('access denied');
 /** @var \AOE\Newrelic\Service $service */
 $service = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\AOE\Newrelic\Service::class);
 if ($service->getConfiguration('track_page_cache_info')) {
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pages']['frontend'] = '\AOE\Newrelic\Cache\Frontend\VariableFrontend';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pages']['frontend'] = '\AOE\Newrelic\Cache\Frontend\VariableFrontend';
 }
 $service->setConfiguredAppName();
 if ($service->getConfiguration('prepend_context')) {
-$service->setTransactionNameDefault('Base');
+    $service->setTransactionNameDefault('Base');
 }
 $service->addCommonRequestParameters();
 
@@ -25,19 +25,19 @@ if (defined('TYPO3_cliMode') && TYPO3_cliMode && $service->getConfiguration('pre
 }
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ($service->getConfiguration('prepend_context')) {
-    $service->addTransactionNamePostfix('FORMSUBMIT');
-	} else {
-		$service->setTransactionName('FORMSUBMIT');
-}
+    if ($service->getConfiguration('prepend_context')) {
+        $service->addTransactionNamePostfix('FORMSUBMIT');
+    } else {
+        $service->setTransactionName('FORMSUBMIT');
+    }
 }
 if (TYPO3_MODE === 'BE') {
     /** @var \AOE\Newrelic\Service $service */
-	$service = GeneralUtility::makeInstance('AOE\Newrelic\Service');
+    $service = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('AOE\Newrelic\Service');
     $service->setConfiguredAppName();
-	if ($service->getConfiguration('prepend_context')) {
-    $service->setTransactionName('Backend');
- }
+    if ($service->getConfiguration('prepend_context')) {
+        $service->setTransactionName('Backend');
+    }
 }
 if (TYPO3_MODE === 'FE') {
     $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest']['newrelic'] = \AOE\Newrelic\Hooks::class . '->frontendPreprocessRequest';
